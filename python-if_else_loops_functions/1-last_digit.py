4bbca7e8addcabe418a6f3c723e36caa5e760265
arsicbt <arsinoechobert@gmail.com>
2025-09-02 15:36:22 +0200

    fix: Faute de frappe

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,6 @@
 import random
 number = random.randint(-10000, 10000)
 ld = number % 10 if number > 0 else number %-10
-msg = "greater than 5" if ld > 5 else "0" if ld == 0 else "less than 6 and not 0"
-
-print(f"Last digit of {number} id {ld} and is {msg}")
+msg = "greater than 5" if ld > 5 else "0"\
+ if ld == 0 else "less than 6 and not 0"
+print(f"Last digit of {number} is {ld} and is {msg}")
