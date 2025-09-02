bc987f5f23ee100d8998e278a3daf191dcb785a2
arsicbt <arsinoechobert@gmail.com>
2025-09-02 15:23:18 +0200

    feat: Create a script that check the last digit of a number and compare it

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+ld = number % 10 if number > 0 else number %-10
+
+print(f"Last digit of {number} is {ld}, and is " 
+ f"{"greater than 5" if ld > 5 else "0" if ld == 0 else "less than 6 and not 0"}")
