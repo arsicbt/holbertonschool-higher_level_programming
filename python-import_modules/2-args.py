3ef57509b8884ccc68285c2323cc62fd2beae939
arsicbt <arsinoechobert@gmail.com>
2025-09-04 13:15:36 +0200

    fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,11 +4,15 @@ if __name__ == "__main__":
     argv = sys.argv
     argc = len(argv) - 1
 
-    print(
-            "0 arguments."
-            if argc == 0
-            else "{} arguments: \n{}: {}".format(argc, 1, argv[1])
-        )
+    if argc <= 1:
+        print(
+                "0 arguments."
+                if argc == 0
+                else "{} argument: \n{}: {}".format(argc, 1, argv[1])
+            )
+    else:
+        print("{} arguments: \n{}: {}".format(argc, 1, argv[1]))
+
     for i in range(2, len(argv)):
         print("{}: {}".format(i, argv[i]))
         i += 1
