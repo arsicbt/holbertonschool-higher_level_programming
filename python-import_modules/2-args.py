78315346b85a3fb663ab63a566f77eb1f41af9df
arsicbt <arsinoechobert@gmail.com>
2025-09-04 13:08:43 +0200

    prints the number of and the list of its arguments.

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    argc = len(argv) - 1
+
+    print(
+            "0 arguments."
+            if argc == 0
+            else "{} arguments: \n{}: {}".format(argc, 1, argv[1])
+        )
+    for i in range(2, len(argv)):
+        print("{}: {}".format(i, argv[i]))
+        i += 1
