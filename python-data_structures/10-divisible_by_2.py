9376bf43019eb8f3db5b14a484a16485f253711c
arsicbt <arsinoechobert@gmail.com>
2025-09-08 14:47:05 +0200

    Classical syntaxe

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,5 +2,8 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for i in my_list:
-        new_list.append(True) if my_list[i] % 2 == 0 else new_list.append(False)
+        if my_list[i] % 2 == 0:
+            new_list.append(True)  
+        else:
+            new_list.append(False)
     return new_list
