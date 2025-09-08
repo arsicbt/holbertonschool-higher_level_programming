36a0289c6e8896dc05323e2182cd1cf37de2f2ee
arsicbt <arsinoechobert@gmail.com>
2025-09-08 10:20:56 +0200

    fix: Handle the idx 0

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < len(my_list):
-        for i in my_list:
-            if idx == i:
-                return my_list[idx]
+        return my_list[idx]
     else:
         return None
