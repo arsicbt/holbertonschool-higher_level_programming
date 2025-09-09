04f67a86eacbbe6232d0958df1a152c0f2666637
arsicbt <arsinoechobert@gmail.com>
2025-09-09 14:19:47 +0200

    fix Syntax Error

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 def roman_to_int(roman_string):
     """ Renvoie l'ecriture decimale du nombre donnee en chiffres
     romains """
-    if roman_string == "" pr is None:
+    if roman_string == "" or is None:
         return 0
     if len(roman_string) == 1:
         return roman[roman_string]
