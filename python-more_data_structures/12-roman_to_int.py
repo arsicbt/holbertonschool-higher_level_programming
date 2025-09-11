3e93d200ebf6cbc27dc8186edbb032823bb4b91b
arsicbt <arsinoechobert@gmail.com>
2025-09-11 14:36:28 +0200

    Retour en arrière

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,7 @@ roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 def roman_to_int(roman_string):
     """ Renvoie l'ecriture decimale du nombre donnee en chiffres
     romains """
-    if not isinstance(roman_string, str) or roman_string == ""
-    or roman_string is None:
+    if roman_string == "" or roman_string is None:
         return 0
     if len(roman_string) == 1:
         return roman[roman_string]
