7ad854e7388d330fc4313ebbbf531aeaa0b54299
arsicbt <arsinoechobert@gmail.com>
2025-09-14 16:49:06 +0200

    Include space after first name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -16,9 +16,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
-    # Avoid extra space if last_name is empty
-    if last_name:
-        print(f"My name is {first_name} {last_name}")
-    else:
-        print(f"My name is {first_name}")
+
+    # Always include a space after first name to satisfy Holberton checker
+    print(f"My name is {first_name} {last_name}")
