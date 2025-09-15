882e0439b7520c16ab622ddc2017f4702003fbc6
arsicbt <arsinoechobert@gmail.com>
2025-09-15 15:08:15 +0200

    Change condition for errors messages

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if self.__width <= 0:
+        if self.__width < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -33,6 +33,6 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if self.__width <= 0:
+        if self.__width < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
