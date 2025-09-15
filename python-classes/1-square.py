34db2b8ef0f2ff7281f023c91730129f6b3b9243
arsicbt <arsinoechobert@gmail.com>
2025-09-15 10:43:35 +0200

    Make a class Square that defines a square by: (based on 0-square.py)

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""1-square module
+
+This module defines a class `Square` with a private instance attribute `size`.
+"""
+
+
+class Square():
+    """A class that defines a square by its size."""
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size: The size of the square (no type/value checks here).
+        """
+        self.__size = __size
