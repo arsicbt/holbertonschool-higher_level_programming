9d6fd1cff07812e35449ec3c151bb8c09d0841a9
arsicbt <arsinoechobert@gmail.com>
2025-09-22 09:43:41 +0200

    fix: Documentions

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""
+Module 0-lookup
+This module give an function that display
+a list of available attributes and methods of an object.
+"""
+
+
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        list: A list of attribute and method names available for the object.
+    """
     return dir(obj)
