38a238d1792b53b666eef0b79222f36a2bbfbbcb
arsicbt <arsinoechobert@gmail.com>
2025-09-29 17:25:31 +0200

    fix: Pycodestyle

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Module --> Read a file """
 
+
 def read_file(filename=""):
+    """ Read a file with UTF-8 encoding """
     with open(filename, 'r', encoding='utf-8') as fichier:
         print(fichier.read(), end="")
