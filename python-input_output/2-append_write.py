4aea9322218dee9f41d8433498a6403061967be5
arsicbt <arsinoechobert@gmail.com>
2025-09-29 21:37:03 +0200

    Appends a string at the end of a text file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 
+Module --> Countain a function that appends 
+a string at the end of a text file 
+"""
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
