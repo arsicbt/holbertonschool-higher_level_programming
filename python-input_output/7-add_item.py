2aa06973b0c54895131aafc5a80f94ddcf17084f
arsicbt <arsinoechobert@gmail.com>
2025-09-30 21:54:00 +0200

    Write a class Student that defines a student by: (based on 9-student.py)

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,11 +7,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-if __name__ == "__main__":
-    try:
-        my_list = load_from_json_file(filename)
-    except FileNotFoundError:
-        my_list = []
 
-    my_list.extend(sys.argv[1:])
-    save_to_json_file(my_list, filename)
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+save_to_json_file(my_list, filename)
