26b49cd015907d9572310b4083c4a94480bb8e14
arsicbt <arsinoechobert@gmail.com>
2025-10-01 17:26:16 +0200

    fix: Python Coding Style

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -14,17 +14,16 @@ def serialize_to_xml(dictionary, filename):
 
     root = ET.Element('data')
 
-    with open(filename, 'r', encoding='utf-8') as f:
-        # Iterate through the dictionary items
-        for k, v in dictionary.items():
-            # Add them as child elements to the root
-            child = ET.SubElement(root, k)
-            # Handle types conversions
-            child.text = str(v)
+    """ Iterate through the dictionary items """
+    for k, v in dictionary.items():
+        """ Add them as child elements to the root """
+        child = ET.SubElement(root, k)
+        """ Handle types conversions """
+        child.text = str(v)
 
-        tree = ET.ElementTree(root)
-        # Write the XML tree to the provided filename
-        tree.write(filename, encoding="utf-8")
+    tree = ET.ElementTree(root)
+    """ Write the XML tree to the provided filename """
+    tree.write(filename, encoding="utf-8")
 
     return tree
 
@@ -32,14 +31,14 @@ def serialize_to_xml(dictionary, filename):
 def deserialize_from_xml(filename):
     """ Deserialize XML content to Python dictionary """
 
-    # Parse the XML file
+    """ Parse the XML file """
     tree = ET.parse(filename)
     root = tree.getroot()
 
     dictionary = {}
-    # Navigate through the XML elements
+    """ Navigate through the XML elements """
     for child in root:
-        # Reconstruct the dictionary
+        """ Reconstruct the dictionary """
         dictionary[child.tag] = child.text
 
     return dictionary
