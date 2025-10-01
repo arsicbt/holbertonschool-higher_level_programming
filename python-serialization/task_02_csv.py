46dce9855c7c8fae0f1b2fe9a66801379a54ab93
arsicbt <arsinoechobert@gmail.com>
2025-10-01 16:29:43 +0200

    fix: Remove the extension convertion

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -12,17 +12,12 @@ import json
 def convert_csv_to_json(filename):
     """ Convert CSV file to JSON """
 
-    if filename.endswith('.csv'):
-        json_filename = filename.replace('.csv', '.json')
-    else:
-        json_filename = filename + '.json'
-
     try:
         with open(filename, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
             data = list(reader)
 
-        with open(json_filename, 'w') as jsonfile:
+        with open('data.json', 'w') as jsonfile:
             json.dump(data, jsonfile)
         
         return True
