0de6bf7378c69bfd2df592c9824597e06d431e8a
arsicbt <arsinoechobert@gmail.com>
2025-10-06 15:56:29 +0200

    fix: Add a dictionary to filters the index that I need

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -13,19 +13,24 @@ def fetch_and_print_posts():
         posts = response_get.json()
         for posts in posts:
             print(posts['title'])
-    return response_get
+
 
 
 def fetch_and_save_posts():
 
-    response_post = fetch_and_print_posts()
+    response_post = requests.get('https://jsonplaceholder.typicode.com/posts')
 
     if response_post.status_code == 200:
         posts = response_post.json()
 
+    clean_posts = [
+            {'id': post['id'], 'title': post['title'], 'body': post['body']}
+            for post in posts
+        ]
+
     with open('response_post', 'w', encoding='utf-8') as file:
 
-        field = posts[0].keys()
+        field = ['id', 'title', 'body']
         writer = csv.DictWriter(file, fieldnames=field)
         writer.writeheader()
-        writer.writerows(posts)
+        writer.writerows(clean_posts)
