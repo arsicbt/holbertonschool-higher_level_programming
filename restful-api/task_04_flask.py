33804c66b55280f704c88a03991df8714f0c77f3
arsicbt <arsinoechobert@gmail.com>
2025-10-07 16:54:55 +0200

    first commit

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -5,12 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
+# décorateur qui associe une URL à une fonction
+@app.route('/')
 def home():
-
     msg = "Welcome to the Flask API!"
     return msg
 
 if __name__ == "__main__": 
     app.run()
-
