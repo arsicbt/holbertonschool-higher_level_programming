df69dc09b947d5b2a11df930cab5e6745ede028e
arsicbt <arsinoechobert@gmail.com>
2025-10-07 18:31:13 +0200

    backup

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,15 +1,47 @@
 #!/usr/bin/python3
 
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+users = [
+    {"username": 1, "name": "khalil"},
+    {"username": 2, "name": "arsinoe"},
+    {"username": 3, "name": "philemon"}
+]
 
 # décorateur qui associe une URL à une fonction
 @app.route('/')
 def home():
     msg = "Welcome to the Flask API!"
     return msg
+
+@app.route('/data')
+def data():
+    user_id = [i['id'] for i in users]
+    extract_id = jsonify(user_id)
+    return extract_id
+
+@app.route('/status')
+def status():
+    return abort(200, description="OK")
+
+@app.route('/users/<username>')
+def show_user(username):
+    response = "User profil:"
+    return response, username
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    new_user_data = request.get_json()
+
+    new_user = {
+        "username": new_user_data.get('username'),
+        "name": new_user_data.get('name')
+    }
+
+    users.append(new_user)
+    return jsonify({201, "New user created!"})
 
 if __name__ == "__main__": 
     app.run()
