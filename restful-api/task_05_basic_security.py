4130fab33b41d82e263deef0e49b18ecc3adbdce
arsicbt <arsinoechobert@gmail.com>
2025-10-08 13:41:51 +0200

    install Flask-JWT-Extended

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+from flask import Flask, jsonify
+from werkzeug.security import generate_password_hash
+
+app = Flask(__name__)
 
 
+# User database, hashed passwords
+users = {
+    "admin": generate_password_hash("admin123"),
+    "user1": generate_password_hash("password1"),
+    "user2": generate_password_hash("password2")
+}
+
+# protect rout
+@auth.login_required
