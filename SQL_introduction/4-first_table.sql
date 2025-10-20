5504173f992f94de7c16a3fb9abfe1d678dfd094
arsicbt <arsinoechobert@gmail.com>
2025-10-20 15:10:20 +0200

    Make a script table called first_table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- Create a table in the current database in my SQL server
-CREATE TABLE first_table (
+CREATE TABLE IF  NOT EXISTS first_table (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOY NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMPS,
-    PROMARY KEY (id)
-)
+    name VARCHAR(256) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
