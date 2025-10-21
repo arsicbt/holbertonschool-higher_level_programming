5cf01a7fd420945d9a7992b4ac2f73cdfb76170f
arsicbt <arsinoechobert@gmail.com>
2025-10-21 16:06:25 +0200

    Make it more minimalist

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,3 @@
 -- Lists all privileges of the MySQL server users
-
--- Creates users
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Allows privileges with grant
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Show privileges
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
