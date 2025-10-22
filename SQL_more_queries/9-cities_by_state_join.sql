41fda01e4c00ee720e9dca810fddf9de55bf76e6
arsicbt <arsinoechobert@gmail.com>
2025-10-22 16:54:24 +0200

    Fix

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
--- Lister les villes
+-- Lister les villes de la DB
 SELECT cities.id, cities.name, states.name
-FROM cities, statesWHERE cities.state_id = states.id
+FROM cities, states
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
