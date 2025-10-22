1e84c261185a077e206e4557d4e7ec32919f6616
arsicbt <arsinoechobert@gmail.com>
2025-10-22 11:11:56 +0200

    Correct the syntaxe

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_02_2;
 
 -- Creates user with password
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant only select privilege on the DB
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
