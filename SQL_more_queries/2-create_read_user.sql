400bdf12761b336ba421560777650530c7af8c36
arsicbt <arsinoechobert@gmail.com>
2025-10-22 14:15:14 +0200

    Correct syntaxe

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,13 +1,10 @@
 -- Creates DB 'hbtn_od_2'
-CREATE DATABASE IF NOT EXISTS hbtn_02_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Creates user with password
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- Supprime les privileges existants
-REVOKE ALL PRIVILAGES, GRANT OPTION FROM 'user_0d_2'@'localhost'
-
--- Grant only select privilege on the DB
+-- Ajoute les privileges selctionner dans la DB
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
 -- Applique les changements
