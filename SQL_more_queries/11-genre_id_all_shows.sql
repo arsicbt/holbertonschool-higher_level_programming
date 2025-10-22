77c5971b16ddca4c4789edf2e09428dfaf5d4198
arsicbt <arsinoechobert@gmail.com>
2025-10-22 17:16:19 +0200

    Fix

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
 -- Lister tout les shows, y compris ceux qui n'ont pas de genre
-SELECT tv_shows.title, tv_show_genres_genre_id
-FROM tv_show_genresLEFT JOIN tv_show_genres
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
