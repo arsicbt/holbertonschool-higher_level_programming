8f653ef53945046edb07a44d2d9dbf005e74948e
arsicbt <arsinoechobert@gmail.com>
2025-10-23 11:23:35 +0200

    lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Répertorie les genre de la DB et affiche le nombre d'émissions liées à chacun
+SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM genres
+INNER JOIN tv_show_genres
+    ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.id
+ORDER BY number_of_shows DESC;
