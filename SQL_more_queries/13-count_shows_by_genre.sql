278d40240822e612b8eb9f8ec6764d90dd89f3ce
arsicbt <arsinoechobert@gmail.com>
2025-10-23 13:47:30 +0200

    Import DB correctly

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,7 @@
 -- Répertorie les genre de la DB et affiche le nombre d'émissions liées à chacun
-SELECT 
-    g.name AS genre,
-    COUNT(DISTINCT s.show_id) AS number_of_shows
-FROM tv_genres g
-JOIN tv_show_genres s
-    ON g.id = s.genre_id
-GROUP BY g.id
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
