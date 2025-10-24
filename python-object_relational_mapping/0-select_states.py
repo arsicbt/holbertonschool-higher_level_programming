1c539b3d72211be4d918e05f5d9a336e83cbc5e5
arsicbt <arsinoechobert@gmail.com>
2025-10-24 16:47:12 +0200

    fix the requirements

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,41 +1,38 @@
-#!/bin/env python3
-"""
-Lists all states of the DB
-"""
-
+#!/usr/bin/python3
+"""Module that lists all states from the database passed as argument."""
 
 import sys
 import MySQLdb
 
 
 def list_states():
-    """Lists states ordered by id"""
+    """Connects to a MySQL database and lists all states ordered by id."""
     # Keep arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # Conection to DB
+    # Connection to DB
     db = MySQLdb.connect(
-        host='Localhost',
+        host="localhost",
         port=3306,
         user=username,
         passwd=password,
         db=database
     )
 
-    # Cursor to execute requests
+    # Cursor to execute queries
     cursor = db.cursor()
 
-    # SQL requests
-    cursor.execute("SELECT * FROM states ORDER by id ASC")
+    # SQL query
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Keeps and displays the result
+    # Display results
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
-    # Closes the cursor and connection
+    # Close cursor and connection
     cursor.close()
     db.close()
 
