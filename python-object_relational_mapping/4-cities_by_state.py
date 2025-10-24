c0f06c835184db2e04fa48116ae21ecbecfda49e
arsicbt <arsinoechobert@gmail.com>
2025-10-24 18:57:03 +0200

    Python coding Style

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -26,9 +26,9 @@ def list_cities():
 
     # SQL query
     cursor.execute("""
-        SELECT cities.id, cities.name, states.name 
-        FROM cities 
-        JOIN states ON cities.state_id = states.id 
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
         ORDER BY cities.id ASC
     """)
 
