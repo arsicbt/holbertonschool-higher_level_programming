221bc8d3a462898c72ee932f130ab00e3e65b116
arsicbt <arsinoechobert@gmail.com>
2025-10-25 17:00:38 +0200

    Shebang

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Changes the name of the State object with id = 2 to "New Mexico"
 in the database hbtn_0e_6_usa.
-
-Usage: ./12-model_state_update_id_2.py <mysql_username> <mysql_password> <database_name>
 """
 
 import sys
