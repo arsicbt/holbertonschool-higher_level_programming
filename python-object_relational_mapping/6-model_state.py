635cde8bd286a317d635c9f0f7560162af14ef87
arsicbt <arsinoechobert@gmail.com>
2025-10-25 15:45:42 +0200

    Python coding style

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -9,18 +9,18 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     engine = create_engine(
         f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}',
         pool_pre_ping=True
     )
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     states = session.query(State).order_by(State.id).all()
-    
+
     for state in states:
         print(f"{state.id}: {state.name}")
-    
+
     session.close()
