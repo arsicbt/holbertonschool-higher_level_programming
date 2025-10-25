6257e210c04cb8c1264a9e5354f4245917e75cca
arsicbt <arsinoechobert@gmail.com>
2025-10-25 15:46:32 +0200

    Python codng Style

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -14,14 +14,14 @@ Base = declarative_base()
 class State(Base):
     """
     Classe State qui représente la table 'states'
-    
+
     Attributes:
         id (int): Clé primaire auto-incrémentée
         name (str): Nom de l'état (max 128 caractères)
     """
     # Nom de la table dans la base de données
     __tablename__ = 'states'
-    
+
     # Colonnes de la table
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
