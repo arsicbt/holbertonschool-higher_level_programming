f30d0e7a7387c08fb61740ed42b03870312f22c9
arsicbt <arsinoechobert@gmail.com>
2025-11-06 16:28:01 +0100

    Factorial

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const n = Number(process.argv[2]);
+
+function factorial (n) {
+  if (isNaN(n) || n < 0) {
+    return 'Not a number';
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(n));
