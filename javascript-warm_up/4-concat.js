c159310bf4457010940575bc716fd551c0bc9cfb
arsicbt <arsinoechobert@gmail.com>
2025-11-06 14:19:40 +0100

    Convert float to int

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv.length <= 2) {
-    console.log("undefined is undefined")
+  console.log('undefined is undefined');
 } else {
-    console.log(process.argv[2].concat(" is"), process.argv[3])
+  console.log(process.argv[2].concat(' is'), process.argv[3]);
 }
--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 
-if 
+const arg = process.argv[2];
+
+if (!arg || isNaN(arg)) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', Math.floor(arg));
+}
