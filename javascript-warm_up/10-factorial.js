c7bef1f133af0a13178d449d18f7bf70e963039c
arsicbt <arsinoechobert@gmail.com>
2025-11-06 16:30:24 +0100

    Adapt for the consign

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,9 +4,6 @@ const n = Number(process.argv[2]);
 
 function factorial (n) {
   if (isNaN(n) || n <= 0) {
-    return 'Not a number';
-  }
-  if (n === 0 || n === 1) {
     return 1;
   }
   return n * factorial(n - 1);
