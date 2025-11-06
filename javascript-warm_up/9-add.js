f326f891ef9f3676f5093008aedd1f398a88738a
arsicbt <arsinoechobert@gmail.com>
2025-11-06 15:30:05 +0100

    Remove condition

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,9 +4,6 @@ const num1 = Number(process.argv[2]);
 const num2 = Number(process.argv[3]);
 
 function add (num1, num2) {
-  if (!num1 || !num2) {
-    console.log('NaN');
-  }
   console.log(num1 + num2);
 }
 
