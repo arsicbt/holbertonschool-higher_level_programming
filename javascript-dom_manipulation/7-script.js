3e3db15defdc989930bcb979ce7e8682f2dc5a3f
arsicbt <arsinoechobert@gmail.com>
2025-11-10 13:43:56 +0100

    Star Wars movies & Say Hello!

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,2 +1,25 @@
 #!/usr/bin/node
 
+document.addEventListener('DOMContentLoaded', async function () {
+  const listMovie = document.querySelector('#list_movies');
+  if (!listMovie) return;
+
+  const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    // Création d'une liste des titres de film dans 'ul'
+    const data = await response.json();
+    data.results.forEach(film => {
+      const li = document.createElement('li');
+      li.textContent = film.title;
+      listMovie.appendChild(li);
+    });
+  } catch (error) {
+    console.error('Erreur lors de la récupération des films :', error);
+  }
+});
--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,2 +1,21 @@
 #!/usr/bin/node
 
+// Chargement du DOM
+document.addEventListener('DOMContentLoaded', async function () {
+  const helloElmt = document.querySelector('#hello');
+  if (!helloElmt) return;
+
+  const url = 'https://hellosalut.stefanbohacek.com/?lang=fr';
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    helloElmt.textContent = data.hello;
+  } catch (error) {
+    console.error('Erreur lors du fetch :', error);
+  }
+});
