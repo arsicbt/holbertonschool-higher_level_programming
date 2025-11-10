86f612fc674d6664dee6634def6e2254040b2353
arsicbt <arsinoechobert@gmail.com>
2025-11-10 10:32:19 +0100

    Toggle classes

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,2 +1,19 @@
 #!/usr/bin/node
 
+// Chargement du DOM
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleDiv = document.querySelector('#toggle_header'); // Je recupere l'element cliquable
+  const header = document.querySelector('header'); // Et l'element que je veux modifier
+
+  toggleDiv.addEventListener('click', function () { // Ecoute du clic
+    if (header.classList.contains('red')) { // Passer du rouge au vert
+      header.classList.remove('red');
+      header.classList.add('green');
+    } else if (header.classList.countains('green')) { // Passer du vert au rouge (reverse)
+      header.classList.remove('green');
+      header.classList('red');
+    } else { // Cas vide, passage au rouge
+      header.classList.add('red');
+    }
+  });
+});
