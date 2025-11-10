104095c6754d569d95c79c2f36700d0fa8fe10c5
arsicbt <arsinoechobert@gmail.com>
2025-11-10 09:51:37 +0100

    Click and turn red

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
 
-// Chargement du DOM
-document.querySelector("red_header").addEventListener("click", function () {
-  // Je recupere l'element avec son ID
-  const redHeaderDiv = document.querySelector('#red_header');
-
-  // Est ce que l'element existe ?
-  if (redHeaderDiv) {
-    redHeaderDiv.addEventListener("click", function () {
-      // Changement de la couleur en rouge
-      const header = document.querySelector('header');
-      if (header) {
-        header.style.color = '#FF0000'
-      }
-   });
-}});
+// Je recupere l'element avec son ID, add
+document.querySelector("#red_header").addEventListener("click", function () {
+  // Changement de la couleur en rouge
+  const header = document.querySelector('header');
+  header.style.color = '#FF0000';
+});
