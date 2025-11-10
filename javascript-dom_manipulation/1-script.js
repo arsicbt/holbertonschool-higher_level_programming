3b6075a609e0116430f6a69870e984483b00e82b
arsicbt <arsinoechobert@gmail.com>
2025-11-10 09:13:54 +0100

    Click and turn red

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
 
+// Je recupere l'element avec son ID, 'addEventListener': ecoute le clic
+document.querySelector('#red_header').addEventListener('click', function () {
+  // Changement de sa couleur en rouge
+  document.querySelector('header').style.color = '#FF0000';
+});
