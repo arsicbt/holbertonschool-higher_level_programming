613bfb0fbd23ff944c4c592ca1f96c8e39441112
arsicbt <arsinoechobert@gmail.com>
2025-11-10 09:47:45 +0100

    Add permissions

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,7 +1,17 @@
 #!/usr/bin/node
 
-// Je recupere l'element avec son ID, 'addEventListener': ecoute le clic
-document.querySelector('#red_header').addEventListener('click', function () {
-  // Changement de sa couleur en rouge
-  document.querySelector('header').style.color = '#FF0000';
-});
+// Chargement du DOM
+document.querySelector("red_header").addEventListener("click", function () {
+  // Je recupere l'element avec son ID
+  const redHeaderDiv = document.querySelector('#red_header');
+
+  // Est ce que l'element existe ?
+  if (redHeaderDiv) {
+    redHeaderDiv.addEventListener("click", function () {
+      // Changement de la couleur en rouge
+      const header = document.querySelector('header');
+      if (header) {
+        header.style.color = '#FF0000'
+      }
+   });
+}});
