b9e209baa5ab8bd718549c5ff48c88d568f829d3
arsicbt <arsinoechobert@gmail.com>
2025-11-10 10:21:30 +0100

    Add `.red` class

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-// Je recupere l'element avec son ID, add
-document.querySelector("#red_header").addEventListener("click", function () {
+// Je recupere l'element avec son ID, addEventListener ecoute le clic de l'user
+document.querySelector('#red_header').addEventListener('click', function () {
   // Changement de la couleur en rouge
   const header = document.querySelector('header');
   header.style.color = '#FF0000';
--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,2 +1,13 @@
 #!/usr/bin/node
 
+// Chargement du DOM
+document.addEventListener('DOMContentLoaded', function () {
+  const redHeaderDiv = document.querySelector('#red_header'); // Je recupere l'element via son ID
+  const header = document.querySelector('header'); // Je recupere l'element header (à modifier)
+
+  if (redHeaderDiv && header) { // Verification de l'existence des champs
+    redHeaderDiv.addEventListener('click', function () { // Ecoute du clic...
+      header.classList.add('red'); // Ajout de la classe red
+    });
+  }
+});
